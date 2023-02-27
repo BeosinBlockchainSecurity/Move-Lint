257202fa7a4e6868b5bbc1cb5632b5a62e926220
leocll <leocll@qq.com>
2023-02-27 17:28:22 +0800

    ~ rename move-lint

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 use clap::Parser;
 
 fn main() {
-    let args = move_ast::Config::parse();
-    // println!("{:#?}", args);
-    let ast = match move_ast::gen_move_ast(args.package_path, args.build_config) {
+    let args = move_lint::Config::parse();
+    let ast = match move_lint::gen_move_ast(args.package_path, args.build_config) {
         Ok(ast) => ast,
         Err(err) => {
             eprintln!("{:#?}", err);
             std::process::exit(1);
         }
     };
-    let issues = match move_ast::move_lint(args.lint_config, &ast) {
+    let issues = match move_lint::move_lint(args.lint_config, &ast) {
         Ok(x) => x,
         Err(err) => {
             eprintln!("{:#?}", err);
