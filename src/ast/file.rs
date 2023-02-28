a734d12438ce79cbd1c2b62f030fa81a7aa1b35c
leocll <leocll@qq.com>
2023-02-28 16:38:27 +0800

    ~ optimize lines for issue

--- src/ast/file.rs
+++ src/ast/file.rs
@@ -9,16 +9,13 @@ use move_command_line_common::files::FileHash;
 pub struct FileSource {
     filename: String,
     content: String,
-    lines: Vec<u32>,
 }
 
 impl FileSource {
     pub fn new(filename: String, content: String) -> Self {
-        let lines = content.clone().split('\n').enumerate().map(|(i, _)|i as u32).collect::<Vec<_>>();
         Self {
             filename,
             content,
-            lines,
         }
     }
 
@@ -35,9 +32,10 @@ impl FileSource {
     }
 
     pub fn get_lines(&self, range: Range<usize>) -> Vec<u32> {
-        let lines_count = self.content[0..range.end].split('\n').collect::<Vec<_>>().len();
-        let range_lines_count = self.content[range].split('\n').collect::<Vec<_>>().len();
-        self.lines[lines_count-range_lines_count..lines_count].to_vec()
+        let lines_count = self.content[range.clone()].split('\n').collect::<Vec<_>>().len() as u32;
+        let start_line = self.content[0..range.end].split('\n').collect::<Vec<_>>().len() as u32 - lines_count + 1;
+        let end_line = start_line + lines_count;
+        (start_line..end_line).collect::<Vec<_>>()
     }
 }
 
